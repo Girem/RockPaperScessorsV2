26f372f2f1de81f9c10523556a5c67f33c66bb1a
Girem <iremashvili21@gmail.com>
2023-02-22 19:16:33 +0400

    displayed score

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const rock = document.querySelector(`.rock`);
 const paper = document.querySelector(`.paper`);
 const scissors = document.querySelector(`.scissors`);
-let playersScore = 0;
-let computersScore = 0;
+const result = document.querySelector(`.result`);
+let playersScore = document.querySelector(`.player_score`);
+let computersScore = document.querySelector(`.computer_score`);
+
+// esaa gasaketebeli aq unda afdeitedes .textcontent
+playersScore = 0;
+computersScore = 0;
 
 rock.addEventListener(`click`, playRound);
 paper.addEventListener(`click`, playRound);
@@ -29,34 +34,42 @@ function playRound(e) {
 
   if (playerSelection === computerSelection) {
     console.log(`you both have selected ` + playerSelection + ` it is a draw`);
+    result.textContent =
+      `you both have selected ` + playerSelection + ` it is a draw`;
     console.log(playerSelection, computerSelection);
   } else if (playerSelection === `rock` && computerSelection === `scissors`) {
     console.log(`you win! Rock bits Scissors`);
+    result.textContent = `you win! Rock bits Scissors`;
     console.log(playerSelection, computerSelection);
     playersScore++;
     return playersScore;
   } else if (playerSelection === `scissors` && computerSelection === `paper`) {
     console.log(`you win! Scissors bits Paper`);
     // console.log(playerSelection, computerSelection);
+    result.textContent = `you win! Scissors bits Paper`;
     playersScore++;
     return playersScore;
   } else if (playerSelection === `paper` && computerSelection === `rock`) {
     console.log(`you win! Paper bits Rock`);
+    result.textContent = `you win! Paper bits Rock`;
     // console.log(playerSelection, computerSelection);
     playersScore++;
     return playersScore;
   } else if (playerSelection === `rock` && computerSelection === `paper`) {
     console.log(`you lose! Paper bits Rock`);
+    result.textContent = `you lose! Paper bits Rock`;
     console.log(playerSelection, computerSelection);
     computersScore++;
     return computersScore;
   } else if (playerSelection === `scissors` && computerSelection === `rock`) {
     console.log(`you lose! Rock bits Scissors`);
+    result.textContent = `you lose! Rock bits Scissors`;
     // console.log(playerSelection, computerSelection);
     computersScore++;
     return computersScore;
   } else if (playerSelection === `paper` && computerSelection === `rock`) {
     console.log(`you lose! Rock bits Paper`);
+    result.textContent = `you lose! Rock bits Paper`;
     // console.log(playerSelection, computerSelection);
     computersScore++;
     return computersScore;
@@ -114,3 +127,6 @@ function playRound(e) {
 // }
 
 // game();
+
+// ვათამაშებთ 5 მდე.
+// მოტამაშე ==5 || კომპ == 5 >> რესულტ “მოგება„ ან „წაგებ“
