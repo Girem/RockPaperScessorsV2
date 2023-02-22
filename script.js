c48906e153cb037f09f4a93b64d1e9a2fa3e1be5
Girem <iremashvili21@gmail.com>
2023-02-22 18:29:59 +0400

    playRound works

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const rock = document.querySelector(`.rock`);
+const paper = document.querySelector(`.paper`);
+const scissors = document.querySelector(`.scissors`);
+let playersScore = 0;
+let computersScore = 0;
+
+rock.addEventListener(`click`, playRound);
+paper.addEventListener(`click`, playRound);
+scissors.addEventListener(`click`, playRound);
 function getComputersChoice() {
   let computersChoice;
 
@@ -12,62 +21,96 @@ function getComputersChoice() {
   return computersChoice;
 }
 
-function getPlayersChoice() {
-  let playersChoice = prompt(`input your choice: rock, paper, of scissors`);
-  return playersChoice.toLocaleLowerCase();
-}
-
-function playRound(playerSelection, computerSelection) {
+function playRound(e) {
   getComputersChoice();
-  playerSelection = getPlayersChoice();
-  computerSelection = getComputersChoice();
+
+  let playerSelection = e.target.className;
+  let computerSelection = getComputersChoice();
 
   if (playerSelection === computerSelection) {
     console.log(`you both have selected ` + playerSelection + ` it is a draw`);
-    // console.log(playerSelection, computerSelection);
+    console.log(playerSelection, computerSelection);
   } else if (playerSelection === `rock` && computerSelection === `scissors`) {
     console.log(`you win! Rock bits Scissors`);
-    // console.log(playerSelection, computerSelection);
-    return playersScore++;
+    console.log(playerSelection, computerSelection);
+    playersScore++;
+    return playersScore;
   } else if (playerSelection === `scissors` && computerSelection === `paper`) {
     console.log(`you win! Scissors bits Paper`);
     // console.log(playerSelection, computerSelection);
-    return playersScore++;
+    playersScore++;
+    return playersScore;
   } else if (playerSelection === `paper` && computerSelection === `rock`) {
     console.log(`you win! Paper bits Rock`);
     // console.log(playerSelection, computerSelection);
-    return playersScore++;
+    playersScore++;
+    return playersScore;
   } else if (playerSelection === `rock` && computerSelection === `paper`) {
     console.log(`you lose! Paper bits Rock`);
-    // console.log(playerSelection, computerSelection);
-    return computersScore++;
+    console.log(playerSelection, computerSelection);
+    computersScore++;
+    return computersScore;
   } else if (playerSelection === `scissors` && computerSelection === `rock`) {
     console.log(`you lose! Rock bits Scissors`);
     // console.log(playerSelection, computerSelection);
-    return computersScore++;
+    computersScore++;
+    return computersScore;
   } else if (playerSelection === `paper` && computerSelection === `rock`) {
     console.log(`you lose! Rock bits Paper`);
     // console.log(playerSelection, computerSelection);
-    return computersScore++;
+    computersScore++;
+    return computersScore;
   }
 }
 
-let playersScore = 0;
-let computersScore = 0;
+// function playRound(playerSelection, computerSelection) {
+//   getComputersChoice();
+//   playerSelection = getPlayersChoice();
+//   computerSelection = getComputersChoice();
 
-function game() {
-  for (i = 0; i < 5; i++) {
-    playRound();
-    console.log(playersScore);
-    console.log(computersScore);
-  }
-  if (playersScore > computersScore) {
-    console.log("You Win!!!");
-  } else if (playersScore == computersScore) {
-    console.log("It is a tie");
-  } else {
-    console.log("You Loose! :( ");
-  }
-}
+//   if (playerSelection === computerSelection) {
+//     console.log(`you both have selected ` + playerSelection + ` it is a draw`);
+//     // console.log(playerSelection, computerSelection);
+//   } else if (playerSelection === `rock` && computerSelection === `scissors`) {
+//     console.log(`you win! Rock bits Scissors`);
+//     // console.log(playerSelection, computerSelection);
+//     return playersScore++;
+//   } else if (playerSelection === `scissors` && computerSelection === `paper`) {
+//     console.log(`you win! Scissors bits Paper`);
+//     // console.log(playerSelection, computerSelection);
+//     return playersScore++;
+//   } else if (playerSelection === `paper` && computerSelection === `rock`) {
+//     console.log(`you win! Paper bits Rock`);
+//     // console.log(playerSelection, computerSelection);
+//     return playersScore++;
+//   } else if (playerSelection === `rock` && computerSelection === `paper`) {
+//     console.log(`you lose! Paper bits Rock`);
+//     // console.log(playerSelection, computerSelection);
+//     return computersScore++;
+//   } else if (playerSelection === `scissors` && computerSelection === `rock`) {
+//     console.log(`you lose! Rock bits Scissors`);
+//     // console.log(playerSelection, computerSelection);
+//     return computersScore++;
+//   } else if (playerSelection === `paper` && computerSelection === `rock`) {
+//     console.log(`you lose! Rock bits Paper`);
+//     // console.log(playerSelection, computerSelection);
+//     return computersScore++;
+//   }
+// }
+
+// function game() {
+//   for (i = 0; i < 5; i++) {
+//     playRound();
+//     console.log(playersScore);
+//     console.log(computersScore);
+//   }
+//   if (playersScore > computersScore) {
+//     console.log("You Win!!!");
+//   } else if (playersScore == computersScore) {
+//     console.log("It is a tie");
+//   } else {
+//     console.log("You Loose! :( ");
+//   }
+// }
 
 // game();
