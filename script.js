2fb481bd351512fea196dafc02ef167f19d5f258
Girem <iremashvili21@gmail.com>
2023-02-22 20:33:28 +0400

    game completed V1

--- script.js
+++ script.js
@@ -28,35 +28,77 @@ function playRound(e) {
   let playerSelection = e.target.className;
   let computerSelection = getComputersChoice();
 
-  if (playerSelection === computerSelection) {
+  if (
+    computersScore.textContent < 5 &&
+    playersScore.textContent < 5 &&
+    playerSelection === computerSelection
+  ) {
     result.textContent =
       `you both have selected ` + playerSelection + ` it is a draw`;
-  } else if (playerSelection === `rock` && computerSelection === `scissors`) {
+  } else if (
+    computersScore.textContent < 5 &&
+    playersScore.textContent < 5 &&
+    playerSelection === `rock` &&
+    computerSelection === `scissors`
+  ) {
     playersScore.textContent++;
 
     result.textContent = `you win! Rock bits Scissors`;
-  } else if (playerSelection === `scissors` && computerSelection === `paper`) {
+  } else if (
+    computersScore.textContent < 5 &&
+    playersScore.textContent < 5 &&
+    playerSelection === `scissors` &&
+    computerSelection === `paper`
+  ) {
     playersScore.textContent++;
 
     result.textContent = `you win! Scissors bits Paper`;
-  } else if (playerSelection === `paper` && computerSelection === `rock`) {
+  } else if (
+    computersScore.textContent < 5 &&
+    playersScore.textContent < 5 &&
+    playerSelection === `paper` &&
+    computerSelection === `rock`
+  ) {
     playersScore.textContent++;
 
     result.textContent = `you win! Paper bits Rock`;
-  } else if (playerSelection === `rock` && computerSelection === `paper`) {
+  } else if (
+    computersScore.textContent < 5 &&
+    playersScore.textContent < 5 &&
+    playerSelection === `rock` &&
+    computerSelection === `paper`
+  ) {
     computersScore.textContent++;
 
     result.textContent = `you lose! Paper bits Rock`;
-  } else if (playerSelection === `scissors` && computerSelection === `rock`) {
+  } else if (
+    computersScore.textContent < 5 &&
+    playersScore.textContent < 5 &&
+    playerSelection === `scissors` &&
+    computerSelection === `rock`
+  ) {
     computersScore.textContent++;
 
     result.textContent = `you lose! Rock bits Scissors`;
-  } else if (playerSelection === `paper` && computerSelection === `scissors`) {
+  } else if (
+    computersScore.textContent < 5 &&
+    playersScore.textContent < 5 &&
+    playerSelection === `paper` &&
+    computerSelection === `scissors`
+  ) {
     computersScore.textContent++;
 
     result.textContent = `you lose! Rock bits Paper`;
+  } else if (computersScore.textContent == 5) {
+    result.textContent = `You loose the game!!`;
+    return;
+  } else if (playersScore.textContent == 5) {
+    result.textContent = `Nice Job!! You win the game!!`;
+    return;
   }
 }
 
 // ვათამაშებთ 5 მდე.
 // მოტამაშე ==5 || კომპ == 5 >> რესულტ “მოგება„ ან „წაგებ“
+
+//
